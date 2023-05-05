3b8dabb8fa98213597ae3ce5d346847fe70b6883
dvirgroh <131441205+dvirgroh@users.noreply.github.com>
2023-05-05 13:20:12 +0300

    Update code.asm

--- code.asm
+++ code.asm
@@ -38,7 +38,15 @@ str db 100
 strlen db 0
 strtxt db 100 dup(0)
 
+
 .CODE
+proc p
+push ax
+lea bx, letterOffset
+add bx,0
+mov [bx], ax
+ret
+endp p 
  
 start:
 mov ax,@data
@@ -46,18 +54,12 @@ mov ds,ax
 
 xor ax,ax 
 xor dx,dx
- 
-;;;;;;;;;;;;  
+
 lea ax, aMorse
-proc p
-lea bx, letterOffset
-add bx,0
-mov [bx], ax
-endp  
+push ax 
+call p 
 
-
-;;;;;;;;;;;
-
+ 
 lea DX,msg1 ;show msg1
 mov AH,09h
 int 21h 
@@ -102,119 +104,6 @@ placeInString:
 mov dl, 61h
 mov al, [strtxt + bx]
 inc bx
-
-cmp al, dl
-JE a
-inc dl
-
-cmp al, dl
-JE b
-inc dl
-
-a: 
-lea dx, aMorse
-mov AH,09h
-int 21h
-b:
-lea dx, bMorse
-mov AH,09h
-int 21h
-c:
-lea dx, cMorse
-mov AH,09h
-int 21h
-d:
-lea dx, dMorse
-mov AH,09h
-int 21h
-e:
-lea dx, eMorse
-mov AH,09h
-int 21h
-f:     
-lea dx, fMorse
-mov AH,09h
-int 21h
-g:     
-lea dx, gMorse
-mov AH,09h
-int 21h
-h:     
-lea dx, hMorse
-mov AH,09h
-int 21h
-i:     
-lea dx, iMorse
-mov AH,09h
-int 21h
-j:     
-lea dx, jMorse
-mov AH,09h
-int 21h
-k:     
-lea dx, kMorse
-mov AH,09h
-int 21h
-l:     
-lea dx, lMorse
-mov AH,09h
-int 21h
-m:     
-lea dx, mMorse
-mov AH,09h
-int 21h
-n: 
-lea dx, nMorse
-mov AH,09h
-int 21h
-o:     
-lea dx, oMorse
-mov AH,09h
-int 21h
-p:     
-lea dx, pMorse
-mov AH,09h
-int 21h
-q:     
-lea dx, qMorse
-mov AH,09h
-int 21h
-r:     
-lea dx, rMorse
-mov AH,09h
-int 21h
-s:     
-lea dx, sMorse
-mov AH,09h
-int 21h
-t:     
-lea dx, tMorse
-mov AH,09h
-int 21h
-u:     
-lea dx, uMorse
-mov AH,09h
-int 21h
-v:     
-lea dx, vMorse
-mov AH,09h
-int 21h
-w:     
-lea dx, wMorse
-mov AH,09h
-int 21h
-x:     
-lea dx, xMorse
-mov AH,09h
-int 21h
-y:     
-lea dx, yMorse
-mov AH,09h
-int 21h
-z:     
-lea dx, zMorse
-mov AH,09h
-int 21h
 
 loop placeInString
 exit:
