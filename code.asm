58d6c5031314d18de71fae075fca75bc6830d214
dvirgroh <131441205+dvirgroh@users.noreply.github.com>
2023-05-24 19:11:06 +0300

    Update code.asm

--- code.asm
+++ code.asm
@@ -4,7 +4,9 @@
 .DATA
 msg1 db 'Enter a string: $' 
 msg2 db 'The string expected is: $'
+msg3 db '# $'
 crlf db 13,10,'$'
+
 aMorse db '.-  $'
 bMorse db '-... $'
 cMorse db '-.-. $'
@@ -31,6 +33,7 @@ wMorse db '.-- $'
 xMorse db '-..- $'
 yMorse db '-.-- $' 
 zMorse db '--.. $'
+
 zeroMorse db '----- $'
 oneMorse db '.---- $'
 twoMorse db '..--- $'
@@ -41,23 +44,31 @@ sixMorse db '-.... $'
 sevenMorse db '--... $'
 eightMorse db '---.. $'
 nineMorse db '----. $'
-quotationMarkMorse db '--..-- $' ; Quotation Mark (")
-numberSignMorse db '.-..-. $' ; Number Sign (#)
+
+spaceMorse db '/$' ;< >
+exclamationmark db '-.-.-- $';<!>
+quotationMarkMorse db '.-..-. $' ; Quotation Mark (")
+;numberSignMorse db ' $' ; Number Sign (#)
 dollarSignMorse db '...-..- $' ; Dollar Sign ($)
 percentSignMorse db '-.-..-. $' ; Percent Sign (%)
 ampersandMorse db '.-... $' ; Ampersand (&)
 singleQuoteMorse db '.----. $' ; Single Quote (')
-asteriskMorse db '-..-.- $' ; Asterisk (*)
+ParenthesisLeft db '-.--. $' ;(
+ParenthesisRight db '-.--.- $';)
+asteriskMorse db '-..-.- $' ; Asterisk (*)          
 plusSignMorse db '.-.-. $' ; Plus Sign (+)
 commaMorse db '--..-- $' ; Comma (,)
 hyphenMorse db '-....- $' ; Hyphen (-)
 periodMorse db '.-.-.- $' ; Period (.)
 forwardSlashMorse db '-..-. $' ; Forward Slash (/)
 
- 
-letterOffset db 55 dup(0)
+colonMorse db '---... $' ; <:>
+equalMorse db '-...- $' ; <=>
+questionMarkMorse db '..--.. $' ;<?>
+strudelMorse db '.--.-. $' ;<@>
 
 
+letterOffset db 100 dup(0)
 str db 100
 strlen db 0
 strtxt db 100 dup(0)
@@ -69,24 +80,44 @@ lea bx, letterOffset
 add bx, cx
 inc cx
 mov [bx], ax
-inc bx
 ret  
 endp p
+
 proc punctuationmarks1
 push ax
 push bx                           ;fix the problem
 push dx 
-add ax,2h
-mov bl,al
-mov bl,[letterOffset+bx]
-mov dx,bx
-mov ah,09h
+sub al, 58
+add al, 16
+mov bl, al
+mov bh, 0
+mov bl, [letterOffset+bx] 
+mov dl, bl
+mov ah, 09h
 int 21h
-pop dx
 pop bx
 pop ax
+pop dx                                                
 ret 6 
-endp punctuationmarks1    
+endp punctuationmarks1
+
+proc punctuationmarks2
+push ax
+push bx                           ;fix the problem
+push dx 
+sub al, 32
+add al, 36
+mov bl, al
+mov bh, 0
+mov bl, [letterOffset+bx] 
+mov dl, bl
+mov ah, 09h
+int 21h
+pop bx
+pop ax
+pop dx                                                
+ret 6 
+endp punctuationmarks2
 
 proc capital  
 add ax,20h
@@ -108,10 +139,11 @@ pop bx
 pop ax
 ret 6 
 endp number
+
 proc printLetter
 push ax
 push bx
-push dx                      ;Do a checker  for /,.   
+push dx                        
 sub al,61h
 mov bl,al
 mov bl,[letterOffset+bx]
@@ -166,104 +198,183 @@ int 21h
 xor ax,ax 
 xor dx,dx
 xor cx,cx
+
 lea ax, aMorse
 call p 
+
 lea ax, bMorse
-call p 
+call p
+ 
 lea ax, cMorse
-call p 
+call p
+ 
 lea ax, dMorse
-call p 
+call p
+ 
 lea ax, eMorse
-call p 
+call p
+ 
 lea ax, fMorse
 call p 
 lea ax, gMorse
 call p 
+
 lea ax, hMorse
 call p 
+
 lea ax, iMorse
-call p 
+call p
+ 
 lea ax, jMorse
 call p 
+
 lea ax, kMorse
 call p 
+
 lea ax, lMorse
 call p 
+
 lea ax, mMorse
 call p 
+
 lea ax, nMorse
 call p 
+
 lea ax, oMorse
 call p 
+
 lea ax, pMorse
 call p 
+
 lea ax, qMorse
 call p 
+
 lea ax, rMorse
 call p 
+
 lea ax, sMorse
 call p 
+
 lea ax, tMorse
 call p 
+
 lea ax, uMorse
 call p 
+
 lea ax, vMorse
 call p 
+
 lea ax, wMorse
 call p 
+
 lea ax, xMorse
 call p
+
 lea ax, yMorse
 call p 
+
 lea ax, zMorse
 call p
+
 lea ax, zeroMorse
 call p
+
 lea ax, oneMorse
 call p
+
 lea ax, twoMorse
 call p
+
 lea ax, threeMorse
 call p
+
 lea ax, fourMorse
 call p
+
 lea ax, fiveMorse
 call p
+
 lea ax, sixMorse
 call p
+
 lea ax, sevenMorse
 call p
+
 lea ax, eightMorse
 call p
+
 lea ax, nineMorse
 call p
-lea ax, quotationMarkMorse
-call p
-lea ax, numberSignMorse
-call p
-lea ax, dollarSignMorse
-call p
-lea ax, percentSignMorse
-call p
-lea ax, ampersandMorse
-call p
-lea ax, singleQuoteMorse
-call p
-lea ax, asteriskMorse
-call p
-lea ax, plusSignMorse
-call p
-lea ax, commaMorse
-call p
-lea ax, hyphenMorse
-call p
-lea ax, periodMorse
-call p
-lea ax, forwardSlashMorse
+
+lea ax, spaceMorse
 call p
 
- 
+lea ax, exclamationmark
+call p
+
+lea ax, quotationMarkMorse  
+call p
+
+lea ax, msg3
+call p
+
+lea ax, dollarSignMorse
+call p
+
+lea ax, percentSignMorse
+call p
+
+lea ax, ampersandMorse
+call p
+
+lea ax, singleQuoteMorse
+call p 
+
+lea ax, ParenthesisLeft
+call p 
+
+lea ax, ParenthesisRight
+call p
+
+lea ax, asteriskMorse
+call p
+
+lea ax, plusSignMorse
+call p
+
+lea ax, commaMorse
+call p
+
+lea ax, hyphenMorse
+call p
+
+lea ax, periodMorse
+call p
+
+lea ax, forwardSlashMorse
+call p  
+
+lea ax, colonMorse
+call p 
+
+lea ax, msg3
+call p
+
+lea ax, msg3
+call p
+
+lea ax, equalMorse
+call p 
+
+lea ax, msg3
+call p
+
+lea ax, questionMarkMorse
+call p 
+
+lea ax, strudelMorse
+call p 
+
 xor cx,cx   
 xor ax,ax 
 xor bx,bx
@@ -274,35 +385,57 @@ mov cl ,strlen
 placeInString:
 mov al, [strtxt + SI]
 inc SI
+
 cmp al,61h
 jb notletter
+
 cmp al,07Ah
 ja notletter 
+
 call printLetter
+
 notletter:
 cmp al,41h
 jb notcapital
+
 cmp al,05Ah
 ja notcapital
+
 call capital
+
 notcapital:
 cmp al,30h
 jb notnumber
+
 cmp al,39h
 ja notnumber
+
 call number
+
 notnumber:
-cmp al,22h
-jb notSign
+cmp al,20h
+jb notSign1
+
 cmp al,02Fh
-ja notSign
+ja notSign1
+
 call punctuationmarks1
-notSign:
+
+notSign1:
+
+cmp al,03Ah
+jb cantBeTranslated
+
+cmp al,40h
+ja cantBeTranslated
+
+cantBeTranslated: 
+
+lea dx, msg3
+mov ah,09h
+int 21h
+
 loop placeInString
-
-
-
-
 
 exit:
 END start
