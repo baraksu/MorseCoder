254ecc224e0fe3c64e98c841f43dd9149f60d8d9
dvirgroh <131441205+dvirgroh@users.noreply.github.com>
2023-06-02 00:03:30 +0300

    Update code.asm

--- code.asm
+++ code.asm
@@ -74,7 +74,7 @@ strudelMorse db '.--.-. $' ;<@>
 letterOffset db 100 dup(0) ;creating an array of the offsets of the vars.
 str db 100 ; the max length of the string
 strlen db 0 ;the actual length of the string
-strtxt db 100 dup(0) ; an array that stores the string.
+strtxt db 0FFh dup(0) ; an array that stores the string.
 
 
 .CODE
